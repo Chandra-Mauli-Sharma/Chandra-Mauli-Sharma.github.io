d249b58627390edb39b0019b15907a68b0cd9592
chandramaulisharma <chandramaulisharma26@gmail.com>
2022-04-21 19:55:42 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import image from "./image.jpeg";
+import github from "./GitHub-Mark-120px-plus.png";
+import linkedin from "./LI-In-Bug.png";
+import insta from "./240px-Instagram_icon.png";
+import twitter from "./logo-twitter-png-5860.png";
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={image} className="Profile-Img" alt="Profile" />
+        <div class="changing-words">
+          <span class="firstName">Chandra Mauli Sharma</span>
+          <br></br>
+          <span class="lastName">Android | Game | Java</span>
+        </div>
+        <div className='Dock'>
+          <div className="row">
+            <div className="column">
+              <a href='https://github.com/Chandra-Mauli-Sharma'><img src={github} alt="github" className='social-links' /></a>
+            </div>
+            <div className="column">
+            <a href='https://www.linkedin.com/in/chandra-mauli-sharma-375b87200/'><img src={linkedin} alt="linkedin" className='social-links' /></a>
+            </div>
+            <div className="column">
+            <a href='https://www.instagram.com/chandra_mauli_sharma/'><img src={insta} alt="insta" className='social-links' /></a>
+            </div>
+            <div className="column">
+            <a href='https://twitter.com/Chandra65827611'><img src={twitter} alt="twitter" className='social-links' /></a>
+            </div>
+          </div>
+        </div>
       </header>
     </div>
   );
